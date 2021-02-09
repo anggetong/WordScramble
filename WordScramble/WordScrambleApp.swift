dfdc887e07e182d3d7ca523e84173f3c5f1c2c85
Arianne Pablo <arianne_escoto@yahoo.com>
2021-02-09 08:10:16 -0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Arianne Pablo on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
